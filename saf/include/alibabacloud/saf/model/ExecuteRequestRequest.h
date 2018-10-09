61820173162b7854439ee0a4652fb9a32f30b4db
yixiong.jxy <yixiong.jxy@alibaba-inc.com>
2018-10-09 21:31:14 +0800

    SAF SDK Auto Released By xinfa.duanxf,Version：1.27.6
    Signed-off-by: yixiong.jxy <yixiong.jxy@alibaba-inc.com>

---
+++ saf/include/alibabacloud/saf/model/ExecuteRequestRequest.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ALIBABACLOUD_SAF_MODEL_EXECUTEREQUESTREQUEST_H_
+#define ALIBABACLOUD_SAF_MODEL_EXECUTEREQUESTREQUEST_H_
+
+#include <string>
+#include <vector>
+#include <alibabacloud/core/RpcServiceRequest.h>
+#include <alibabacloud/saf/SafExport.h>
+
+namespace AlibabaCloud
+{
+	namespace Saf
+	{
+		namespace Model
+		{
+			class ALIBABACLOUD_SAF_EXPORT ExecuteRequestRequest : public RpcServiceRequest
+			{
+
+			public:
+				ExecuteRequestRequest();
+				~ExecuteRequestRequest();
+
+				std::string getServiceParameters()const;
+				void setServiceParameters(const std::string& serviceParameters);
+				std::string getService()const;
+				void setService(const std::string& service);
+
+            private:
+				std::string serviceParameters_;
+				std::string service_;
+
+			};
+		}
+	}
+}
+#endif // !ALIBABACLOUD_SAF_MODEL_EXECUTEREQUESTREQUEST_H_
---
+++ saf/src/model/ExecuteRequestRequest.cc
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <alibabacloud/saf/model/ExecuteRequestRequest.h>
+
+using AlibabaCloud::Saf::Model::ExecuteRequestRequest;
+
+ExecuteRequestRequest::ExecuteRequestRequest() :
+	RpcServiceRequest("saf", "2018-09-19", "ExecuteRequest")
+{}
+
+ExecuteRequestRequest::~ExecuteRequestRequest()
+{}
+
+std::string ExecuteRequestRequest::getServiceParameters()const
+{
+	return serviceParameters_;
+}
+
+void ExecuteRequestRequest::setServiceParameters(const std::string& serviceParameters)
+{
+	serviceParameters_ = serviceParameters;
+	setParameter("ServiceParameters", serviceParameters);
+}
+
+std::string ExecuteRequestRequest::getService()const
+{
+	return service_;
+}
+
+void ExecuteRequestRequest::setService(const std::string& service)
+{
+	service_ = service;
+	setParameter("Service", service);
+}
+
