03c91d546a8b8f9fa8472dfa1537b0c0f83019d6
Jackson Tian <shyvo1987@gmail.com>
2019-01-10 12:15:42 +0800

    fixup

--- core/src/CurlHttpClient.cc
+++ core/src/CurlHttpClient.cc
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009-2017 Alibaba Cloud All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -104,6 +104,7 @@ HttpClient::HttpResponseOutcome CurlHttpClient::makeRequest(const HttpRequest &r
 		break;
 	}
 	curl_easy_setopt(curlHandle_, CURLOPT_URL, url.c_str());
+	curl_easy_setopt(curlHandle_, CURLOPT_VERBOSE, 1);
 	curl_easy_setopt(curlHandle_, CURLOPT_SSL_VERIFYPEER, 1L);
 	curl_easy_setopt(curlHandle_, CURLOPT_SSL_VERIFYHOST, 2L);
 	curl_easy_setopt(curlHandle_, CURLOPT_HEADERDATA, &response);
