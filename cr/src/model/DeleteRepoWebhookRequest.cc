1da6c4aa1835bd55fb348a69dca107859518d357
haowei.yao <haowei.yao@alibaba-inc.com>
2019-03-14 16:22:34 +0800

    CR SDK Auto Released By shenshi,Version：1.34.24
    Signed-off-by: haowei.yao <haowei.yao@alibaba-inc.com>

---
+++ cr/src/model/DeleteRepoWebhookRequest.cc
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <alibabacloud/cr/model/DeleteRepoWebhookRequest.h>
+
+using AlibabaCloud::Cr::Model::DeleteRepoWebhookRequest;
+
+DeleteRepoWebhookRequest::DeleteRepoWebhookRequest() :
+	RoaServiceRequest("cr", "2016-06-07")
+{}
+
+DeleteRepoWebhookRequest::~DeleteRepoWebhookRequest()
+{}
+
+std::string DeleteRepoWebhookRequest::getRepoNamespace()const
+{
+	return repoNamespace_;
+}
+
+void DeleteRepoWebhookRequest::setRepoNamespace(const std::string& repoNamespace)
+{
+	repoNamespace_ = repoNamespace;
+	setParameter("RepoNamespace", repoNamespace);
+}
+
+long DeleteRepoWebhookRequest::getWebhookId()const
+{
+	return webhookId_;
+}
+
+void DeleteRepoWebhookRequest::setWebhookId(long webhookId)
+{
+	webhookId_ = webhookId;
+	setParameter("WebhookId", std::to_string(webhookId));
+}
+
+std::string DeleteRepoWebhookRequest::getRepoName()const
+{
+	return repoName_;
+}
+
+void DeleteRepoWebhookRequest::setRepoName(const std::string& repoName)
+{
+	repoName_ = repoName;
+	setParameter("RepoName", repoName);
+}
+
+std::string DeleteRepoWebhookRequest::getRegionId()const
+{
+	return regionId_;
+}
+
+void DeleteRepoWebhookRequest::setRegionId(const std::string& regionId)
+{
+	regionId_ = regionId;
+	setParameter("RegionId", regionId);
+}
+
---
+++ cr/src/model/GetImageManifestRequest.cc
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <alibabacloud/cr/model/GetImageManifestRequest.h>
+
+using AlibabaCloud::Cr::Model::GetImageManifestRequest;
+
+GetImageManifestRequest::GetImageManifestRequest() :
+	RoaServiceRequest("cr", "2016-06-07")
+{}
+
+GetImageManifestRequest::~GetImageManifestRequest()
+{}
+
+std::string GetImageManifestRequest::getRepoNamespace()const
+{
+	return repoNamespace_;
+}
+
+void GetImageManifestRequest::setRepoNamespace(const std::string& repoNamespace)
+{
+	repoNamespace_ = repoNamespace;
+	setParameter("RepoNamespace", repoNamespace);
+}
+
+std::string GetImageManifestRequest::getRepoName()const
+{
+	return repoName_;
+}
+
+void GetImageManifestRequest::setRepoName(const std::string& repoName)
+{
+	repoName_ = repoName;
+	setParameter("RepoName", repoName);
+}
+
+std::string GetImageManifestRequest::getRegionId()const
+{
+	return regionId_;
+}
+
+void GetImageManifestRequest::setRegionId(const std::string& regionId)
+{
+	regionId_ = regionId;
+	setParameter("RegionId", regionId);
+}
+
+std::string GetImageManifestRequest::getTag()const
+{
+	return tag_;
+}
+
+void GetImageManifestRequest::setTag(const std::string& tag)
+{
+	tag_ = tag;
+	setParameter("Tag", tag);
+}
+
+int GetImageManifestRequest::getSchemaVersion()const
+{
+	return schemaVersion_;
+}
+
+void GetImageManifestRequest::setSchemaVersion(int schemaVersion)
+{
+	schemaVersion_ = schemaVersion;
+	setParameter("SchemaVersion", std::to_string(schemaVersion));
+}
+
