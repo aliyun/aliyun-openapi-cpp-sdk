e7e3f36e6015fd40a35591e7ccdc272f17dc40d1
axios <axiosleo@foxmail.com>
2019-08-10 13:43:03 +0800

    change method of  include "Utils.h"

--- test/core/commonclient_ut.cc
+++ test/core/commonclient_ut.cc
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <stdio.h>
 #include "gtest/gtest.h"
-#include "../../core/src/Utils.h"
+#include <alibabacloud/core/Utils.h>
 #include "alibabacloud/core/Config.h"
 #include "alibabacloud/core/AlibabaCloud.h"
 #include "alibabacloud/core/CommonClient.h"
@@ -10,71 +10,75 @@
 #include "alibabacloud/core/CommonRequest.h"
 #include "alibabacloud/core/SimpleCredentialsProvider.h"
 
-
 using namespace std;
 using namespace AlibabaCloud;
 
-namespace AlibabaCloud {
-  class TestCommonClient : public CommonClient {
-    public:
-      TestCommonClient(const Credentials &credentials, const ClientConfiguration &configuration):
-        CommonClient(credentials, configuration)
-      {}
-      using CommonClient::buildHttpRequest;
-      using CommonClient::buildRoaHttpRequest;
-      using CommonClient::buildRpcHttpRequest;
-      using CommonClient::makeRequest;
-      using CommonClient::asyncExecute;
-      using CommonClient::commonResponseAsync;
-  };
-}
+namespace AlibabaCloud
+{
+class TestCommonClient : public CommonClient
+{
+public:
+  TestCommonClient(const Credentials &credentials, const ClientConfiguration &configuration) : CommonClient(credentials, configuration)
+  {
+  }
+  using CommonClient::asyncExecute;
+  using CommonClient::buildHttpRequest;
+  using CommonClient::buildRoaHttpRequest;
+  using CommonClient::buildRpcHttpRequest;
+  using CommonClient::commonResponseAsync;
+  using CommonClient::makeRequest;
+};
+} // namespace AlibabaCloud
 
 static int nbr = 0;
-static void task() {
-   nbr |= 0x01;
+static void task()
+{
+  nbr |= 0x01;
 }
 
-namespace {
+namespace
+{
 
-  TEST(CommonClient, basic) {
-    InitializeSdk();
-    const ClientConfiguration cfg;
+TEST(CommonClient, basic)
+{
+  InitializeSdk();
+  const ClientConfiguration cfg;
 
-    std::string key    = "accessKeyId";
-    std::string secret = "accessSecret";
-    std::string token  = "sessionToken";
+  std::string key = "accessKeyId";
+  std::string secret = "accessSecret";
+  std::string token = "sessionToken";
 
-    const Credentials credentials(key, secret, token);
+  const Credentials credentials(key, secret, token);
 
-    const CommonClient cc1(credentials, cfg);
-    CommonClient cc2(std::make_shared<SimpleCredentialsProvider>(credentials), cfg);
-    CommonClient cc3(key, secret, cfg);
-    CommonClient cc4(key, secret, cfg);
+  const CommonClient cc1(credentials, cfg);
+  CommonClient cc2(std::make_shared<SimpleCredentialsProvider>(credentials), cfg);
+  CommonClient cc3(key, secret, cfg);
+  CommonClient cc4(key, secret, cfg);
 
-    CommonRequest cr;
-    cr.setContent("test-content", 12);
+  CommonRequest cr;
+  cr.setContent("test-content", 12);
 
-    CommonClient::CommonResponseOutcome out1 = cc1.commonResponse(cr);
-    EXPECT_TRUE(out1.error().errorCode() == "NetworkError");
-    EXPECT_TRUE(out1.error().errorMessage().find("Failed to connect to host or proxy: GET ") == 0);
+  CommonClient::CommonResponseOutcome out1 = cc1.commonResponse(cr);
+  EXPECT_TRUE(out1.error().errorCode() == "NetworkError");
+  EXPECT_TRUE(out1.error().errorMessage().find("Failed to connect to host or proxy: GET ") == 0);
 
-    EXPECT_TRUE(out1.result().payload() == "");
+  EXPECT_TRUE(out1.result().payload() == "");
 
-    CommonClient::CommonResponseOutcome out2 = cc2.commonResponse(cr);
-    EXPECT_TRUE(out2.error().errorCode() == "NetworkError");
-    EXPECT_TRUE(out2.error().errorMessage().find("Failed to connect to host or proxy: GET ") == 0);
-    EXPECT_TRUE(out2.result().payload() == "");
+  CommonClient::CommonResponseOutcome out2 = cc2.commonResponse(cr);
+  EXPECT_TRUE(out2.error().errorCode() == "NetworkError");
+  EXPECT_TRUE(out2.error().errorMessage().find("Failed to connect to host or proxy: GET ") == 0);
+  EXPECT_TRUE(out2.result().payload() == "");
 
-    CommonClient::CommonResponseOutcome out3 = cc3.commonResponse(cr);
-    EXPECT_TRUE(out3.error().errorCode() == "NetworkError");
-    EXPECT_TRUE(out3.error().errorMessage().find("Failed to connect to host or proxy: GET ") == 0);
-    EXPECT_TRUE(out3.result().payload() == "");
+  CommonClient::CommonResponseOutcome out3 = cc3.commonResponse(cr);
+  EXPECT_TRUE(out3.error().errorCode() == "NetworkError");
+  EXPECT_TRUE(out3.error().errorMessage().find("Failed to connect to host or proxy: GET ") == 0);
+  EXPECT_TRUE(out3.result().payload() == "");
 
-    cr.setRequestPattern(CommonRequest::RequestPattern::RoaPattern);
-    CommonClient::CommonResponseOutcome out4 = cc4.commonResponse(cr);
-    EXPECT_TRUE(out4.error().errorCode() == "NetworkError");
-    EXPECT_TRUE(out4.error().errorMessage().find("Failed to connect to host or proxy: GET ") == 0);
-    EXPECT_TRUE(out4.result().payload() == "");
+  cr.setRequestPattern(CommonRequest::RequestPattern::RoaPattern);
+  CommonClient::CommonResponseOutcome out4 = cc4.commonResponse(cr);
+  EXPECT_TRUE(out4.error().errorCode() == "NetworkError");
+  EXPECT_TRUE(out4.error().errorMessage().find("Failed to connect to host or proxy: GET ") == 0);
+  EXPECT_TRUE(out4.result().payload() == "");
 
   /*
   https://cn-hangzhou/?
@@ -89,90 +93,91 @@ namespace {
     &Timestamp=2019-01-02T08%3A56%3A26Z
     &Version=
   */
-    cr.setRequestPattern(CommonRequest::RequestPattern::RpcPattern);
-    TestCommonClient* client = new TestCommonClient(credentials, cfg);
-    HttpRequest r = client->buildHttpRequest("cn-hangzhou", cr, HttpRequest::Method::Post);
-    EXPECT_TRUE(r.method() == HttpRequest::Method::Post);
-    EXPECT_TRUE(r.url().scheme() == "https");
-    EXPECT_TRUE(r.url().userName() == "");
-    EXPECT_TRUE(r.url().password() == "");
-    EXPECT_TRUE(r.url().host() == "cn-hangzhou");
-    EXPECT_TRUE(r.url().port() == -1);
-    EXPECT_TRUE(r.url().fragment() == "");
-    EXPECT_TRUE(r.url().query().find("AccessKeyId=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("Format=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("RegionId=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("SecurityToken=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("Signature=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("SignatureMethod=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("SignatureNonce=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("SignatureVersion=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("Timestamp=") != string::npos);
-    EXPECT_TRUE(r.url().query().find("Version=") != string::npos);
+  cr.setRequestPattern(CommonRequest::RequestPattern::RpcPattern);
+  TestCommonClient *client = new TestCommonClient(credentials, cfg);
+  HttpRequest r = client->buildHttpRequest("cn-hangzhou", cr, HttpRequest::Method::Post);
+  EXPECT_TRUE(r.method() == HttpRequest::Method::Post);
+  EXPECT_TRUE(r.url().scheme() == "https");
+  EXPECT_TRUE(r.url().userName() == "");
+  EXPECT_TRUE(r.url().password() == "");
+  EXPECT_TRUE(r.url().host() == "cn-hangzhou");
+  EXPECT_TRUE(r.url().port() == -1);
+  EXPECT_TRUE(r.url().fragment() == "");
+  EXPECT_TRUE(r.url().query().find("AccessKeyId=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("Format=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("RegionId=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("SecurityToken=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("Signature=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("SignatureMethod=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("SignatureNonce=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("SignatureVersion=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("Timestamp=") != string::npos);
+  EXPECT_TRUE(r.url().query().find("Version=") != string::npos);
 
-    cr.setQueryParameter("query_k1", "query_v1");
-    cr.setHeaderParameter("header_k1", "header_v1");
-    cr.setScheme("");
-    cr.setHeaderParameter("Accept", "");
-    HttpRequest rr = client->buildRoaHttpRequest("cn-shanghai", cr, HttpRequest::Method::Get);
-    EXPECT_TRUE(rr.method() == HttpRequest::Method::Get);
-    EXPECT_TRUE(rr.header("Accept") == "application/json");
-    EXPECT_TRUE(rr.url().toString() == "https://cn-shanghai/?header_k1=header_v1");
-    EXPECT_TRUE(rr.header("Host") == "cn-shanghai");
-    // Wed, 09 Jan 2019 06:32:41 GMT
-    string date = "Wed, 09 Jan 2019 06:32:41 GMT";
-    EXPECT_TRUE(rr.header("Date").length() == date.length());
-    string signature_nonce = "fb62c51d-f735-4ec7-870d-c6ce03368214";
-    EXPECT_TRUE(rr.header("x-acs-signature-nonce").length() == signature_nonce.length());
+  cr.setQueryParameter("query_k1", "query_v1");
+  cr.setHeaderParameter("header_k1", "header_v1");
+  cr.setScheme("");
+  cr.setHeaderParameter("Accept", "");
+  HttpRequest rr = client->buildRoaHttpRequest("cn-shanghai", cr, HttpRequest::Method::Get);
+  EXPECT_TRUE(rr.method() == HttpRequest::Method::Get);
+  EXPECT_TRUE(rr.header("Accept") == "application/json");
+  EXPECT_TRUE(rr.url().toString() == "https://cn-shanghai/?header_k1=header_v1");
+  EXPECT_TRUE(rr.header("Host") == "cn-shanghai");
+  // Wed, 09 Jan 2019 06:32:41 GMT
+  string date = "Wed, 09 Jan 2019 06:32:41 GMT";
+  EXPECT_TRUE(rr.header("Date").length() == date.length());
+  string signature_nonce = "fb62c51d-f735-4ec7-870d-c6ce03368214";
+  EXPECT_TRUE(rr.header("x-acs-signature-nonce").length() == signature_nonce.length());
 
-    EXPECT_TRUE(rr.header("x-acs-signature-method") == "HMAC-SHA1");
-    EXPECT_TRUE(rr.header("x-acs-signature-version") == "1.0");
-    EXPECT_TRUE(rr.header("x-acs-version") == "");
-    EXPECT_TRUE(rr.header("x-sdk-client") == string("CPP/").append(ALIBABACLOUD_VERSION_STR));
-    EXPECT_TRUE(rr.header("x-acs-region-id") == "cn-hangzhou");
-    // acs accessKeyId:JZD81jGWLp1F3ZIkaLp1yuEZmKc=
-    EXPECT_TRUE(rr.header("Authorization").find("acs accessKeyId:") != string::npos);
-    EXPECT_TRUE(rr.header("unknown-header") == "");
+  EXPECT_TRUE(rr.header("x-acs-signature-method") == "HMAC-SHA1");
+  EXPECT_TRUE(rr.header("x-acs-signature-version") == "1.0");
+  EXPECT_TRUE(rr.header("x-acs-version") == "");
+  EXPECT_TRUE(rr.header("x-sdk-client") == string("CPP/").append(ALIBABACLOUD_VERSION_STR));
+  EXPECT_TRUE(rr.header("x-acs-region-id") == "cn-hangzhou");
+  // acs accessKeyId:JZD81jGWLp1F3ZIkaLp1yuEZmKc=
+  EXPECT_TRUE(rr.header("Authorization").find("acs accessKeyId:") != string::npos);
+  EXPECT_TRUE(rr.header("unknown-header") == "");
 
-    cr.setScheme("http");
-    cr.setHeaderParameter("Accept", "test-accept");
-    cr.setHeaderParameter("Content-Type", "test-content-type");
-    rr = client->buildRoaHttpRequest("cn-shanghai", cr, HttpRequest::Method::Get);
-    EXPECT_TRUE(rr.url().toString() == "http://cn-shanghai/?Accept=test-accept&Content-Type=test-content-type&header_k1=header_v1");
-    EXPECT_TRUE(rr.header("Accept") == "test-accept");
-    EXPECT_TRUE(rr.header("Content-Type") == "test-content-type");
+  cr.setScheme("http");
+  cr.setHeaderParameter("Accept", "test-accept");
+  cr.setHeaderParameter("Content-Type", "test-content-type");
+  rr = client->buildRoaHttpRequest("cn-shanghai", cr, HttpRequest::Method::Get);
+  EXPECT_TRUE(rr.url().toString() == "http://cn-shanghai/?Accept=test-accept&Content-Type=test-content-type&header_k1=header_v1");
+  EXPECT_TRUE(rr.header("Accept") == "test-accept");
+  EXPECT_TRUE(rr.header("Content-Type") == "test-content-type");
 
-    HttpRequest rrr = client->buildRpcHttpRequest("cn-hangzhou", cr, HttpRequest::Method::Post);
-    EXPECT_TRUE(client->serviceName() == "Common");
-    EXPECT_TRUE(rrr.url().scheme() == "http");
+  HttpRequest rrr = client->buildRpcHttpRequest("cn-hangzhou", cr, HttpRequest::Method::Post);
+  EXPECT_TRUE(client->serviceName() == "Common");
+  EXPECT_TRUE(rrr.url().scheme() == "http");
 
-    cr.setScheme("");
-    rrr = client->buildRpcHttpRequest("cn-hangzhou", cr, HttpRequest::Method::Post);
-    EXPECT_TRUE(rrr.url().scheme() == "https");
+  cr.setScheme("");
+  rrr = client->buildRpcHttpRequest("cn-hangzhou", cr, HttpRequest::Method::Post);
+  EXPECT_TRUE(rrr.url().scheme() == "https");
 
-    std::function<void()> func(task);
-    Runnable* rf = new Runnable(func);
-    EXPECT_TRUE(nbr == 0);
-    client->asyncExecute(rf);
-    usleep(10000);
-    EXPECT_TRUE(nbr == 1);
-    ShutdownSdk();
-  }
+  std::function<void()> func(task);
+  Runnable *rf = new Runnable(func);
+  EXPECT_TRUE(nbr == 0);
+  client->asyncExecute(rf);
+  usleep(10000);
+  EXPECT_TRUE(nbr == 1);
+  ShutdownSdk();
+}
 
-  TEST(CommonClient, abnormal) {
-    InitializeSdk();
+TEST(CommonClient, abnormal)
+{
+  InitializeSdk();
 
-    ClientConfiguration configuration("cn-hangzhou");
-    CommonClient client("key", "secret", configuration);
+  ClientConfiguration configuration("cn-hangzhou");
+  CommonClient client("key", "secret", configuration);
 
-    CommonRequest req;
-    req.setScheme("http");
-    req.setDomain("aliyun.com");
+  CommonRequest req;
+  req.setScheme("http");
+  req.setDomain("aliyun.com");
 
-    auto out = client.commonResponse(req);
-    EXPECT_TRUE(out.error().errorCode() == "InvalidResponse");
-    EXPECT_TRUE(out.result().payload().empty());
-/*
+  auto out = client.commonResponse(req);
+  EXPECT_TRUE(out.error().errorCode() == "InvalidResponse");
+  EXPECT_TRUE(out.result().payload().empty());
+  /*
 1: =====================<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
 1: <html>
 1: <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -183,39 +188,40 @@ namespace {
 1: <hr/>Powered by Tengine/Aserver</body>
 1: </html>
 */
-    EXPECT_FALSE(out.error().errorMessage().empty());
-    ShutdownSdk();
-  }
-
-  void cb(const CommonClient *client,
-          const CommonRequest &req,
-          const CommonClient::CommonResponseOutcome& out,
-          const std::shared_ptr<const AsyncCallerContext>& contex) {
-    EXPECT_FALSE(out.error().errorCode().empty());
-  }
-
-  TEST(CommonClient, async) {
-    InitializeSdk();
-    const ClientConfiguration cfg;
-
-    std::string key    = "accessKeyId";
-    std::string secret = "accessSecret";
-    std::string token  = "sessionToken";
-
-    const Credentials credentials(key, secret, token);
-
-    const CommonClient cc1(credentials, cfg);
-
-    CommonRequest cr;
-    cr.setContent("test-content", 12);
-
-    TestCommonClient::CommonResponseAsyncHandler handler(cb);
-
-
-    const AsyncCallerContext context;
-    cc1.commonResponseAsync(cr, handler, std::make_shared<const AsyncCallerContext>(context));
-
-    ShutdownSdk();
-  }
-
+  EXPECT_FALSE(out.error().errorMessage().empty());
+  ShutdownSdk();
 }
+
+void cb(const CommonClient *client,
+        const CommonRequest &req,
+        const CommonClient::CommonResponseOutcome &out,
+        const std::shared_ptr<const AsyncCallerContext> &contex)
+{
+  EXPECT_FALSE(out.error().errorCode().empty());
+}
+
+TEST(CommonClient, async)
+{
+  InitializeSdk();
+  const ClientConfiguration cfg;
+
+  std::string key = "accessKeyId";
+  std::string secret = "accessSecret";
+  std::string token = "sessionToken";
+
+  const Credentials credentials(key, secret, token);
+
+  const CommonClient cc1(credentials, cfg);
+
+  CommonRequest cr;
+  cr.setContent("test-content", 12);
+
+  TestCommonClient::CommonResponseAsyncHandler handler(cb);
+
+  const AsyncCallerContext context;
+  cc1.commonResponseAsync(cr, handler, std::make_shared<const AsyncCallerContext>(context));
+
+  ShutdownSdk();
+}
+
+} // namespace
