9518fd24466dfaf2e3cc65c4da1af437f530f3b7
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2019-08-23 15:02:06 +0800

    cdn feature test

--- test/function_test/cdn/cdn_describeCdnService_ft.cc
+++ test/function_test/cdn/cdn_describeCdnService_ft.cc
@@ -15,14 +15,13 @@ TEST(cdn, describeCdnService)
   utUtils utils;
   string key = utils.get_env("ENV_AccessKeyId");
   string secret = utils.get_env("ENV_AccessKeySecret");
-  string uid = utils.get_env("ENV_TEST_UID");
   InitializeSdk();
-  // ClientConfiguration configuration("cn-hangzhou");
-  // CdnClient client(key, secret, configuration);
-  // Model::DescribeCdnServiceRequest request;
-  // CdnClient::DescribeCdnServiceOutcome outcome = client.describeCdnService(request);
+  ClientConfiguration configuration("cn-hangzhou");
+  CdnClient client(key, secret, configuration);
+  Model::DescribeCdnServiceRequest request;
+  CdnClient::DescribeCdnServiceOutcome outcome = client.describeCdnService(request);
 
-  // EXPECT_NO_THROW();
+  EXPECT_NO_THROW();
   ShutdownSdk();
 }
 } // namespace
