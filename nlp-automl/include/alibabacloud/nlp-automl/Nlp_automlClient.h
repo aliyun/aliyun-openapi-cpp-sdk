b0c5f8414d4d7cff5267b6bf27efe0c357529535
sdk-team <sdk-team@alibabacloud.com>
2019-11-15 22:31:16 +0800

    Nlp Automl SDK.

---
+++ nlp-automl/include/alibabacloud/nlp-automl/Nlp_automlClient.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ALIBABACLOUD_NLP_AUTOML_NLP_AUTOMLCLIENT_H_
+#define ALIBABACLOUD_NLP_AUTOML_NLP_AUTOMLCLIENT_H_
+
+#include <future>
+#include <alibabacloud/core/AsyncCallerContext.h>
+#include <alibabacloud/core/EndpointProvider.h>
+#include <alibabacloud/core/RpcServiceClient.h>
+#include "Nlp_automlExport.h"
+#include "model/GetPredictResultRequest.h"
+#include "model/GetPredictResultResult.h"
+#include "model/RunContactReviewRequest.h"
+#include "model/RunContactReviewResult.h"
+
+
+namespace AlibabaCloud
+{
+	namespace Nlp_automl
+	{
+		class ALIBABACLOUD_NLP_AUTOML_EXPORT Nlp_automlClient : public RpcServiceClient
+		{
+		public:
+			typedef Outcome<Error, Model::GetPredictResultResult> GetPredictResultOutcome;
+			typedef std::future<GetPredictResultOutcome> GetPredictResultOutcomeCallable;
+			typedef std::function<void(const Nlp_automlClient*, const Model::GetPredictResultRequest&, const GetPredictResultOutcome&, const std::shared_ptr<const AsyncCallerContext>&)> GetPredictResultAsyncHandler;
+			typedef Outcome<Error, Model::RunContactReviewResult> RunContactReviewOutcome;
+			typedef std::future<RunContactReviewOutcome> RunContactReviewOutcomeCallable;
+			typedef std::function<void(const Nlp_automlClient*, const Model::RunContactReviewRequest&, const RunContactReviewOutcome&, const std::shared_ptr<const AsyncCallerContext>&)> RunContactReviewAsyncHandler;
+
+			Nlp_automlClient(const Credentials &credentials, const ClientConfiguration &configuration);
+			Nlp_automlClient(const std::shared_ptr<CredentialsProvider> &credentialsProvider, const ClientConfiguration &configuration);
+			Nlp_automlClient(const std::string &accessKeyId, const std::string &accessKeySecret, const ClientConfiguration &configuration);
+			~Nlp_automlClient();
+			GetPredictResultOutcome getPredictResult(const Model::GetPredictResultRequest &request)const;
+			void getPredictResultAsync(const Model::GetPredictResultRequest& request, const GetPredictResultAsyncHandler& handler, const std::shared_ptr<const AsyncCallerContext>& context = nullptr) const;
+			GetPredictResultOutcomeCallable getPredictResultCallable(const Model::GetPredictResultRequest& request) const;
+			RunContactReviewOutcome runContactReview(const Model::RunContactReviewRequest &request)const;
+			void runContactReviewAsync(const Model::RunContactReviewRequest& request, const RunContactReviewAsyncHandler& handler, const std::shared_ptr<const AsyncCallerContext>& context = nullptr) const;
+			RunContactReviewOutcomeCallable runContactReviewCallable(const Model::RunContactReviewRequest& request) const;
+	
+		private:
+			std::shared_ptr<EndpointProvider> endpointProvider_;
+		};
+	}
+}
+
+#endif // !ALIBABACLOUD_NLP_AUTOML_NLP_AUTOMLCLIENT_H_
