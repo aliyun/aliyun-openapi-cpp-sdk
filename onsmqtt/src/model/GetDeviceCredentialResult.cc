3303c6120deef4aa86267920a84826f0cb6a89b3
sdk-team <sdk-team@alibabacloud.com>
2021-02-01 13:23:11 +0000

    Support device credential for mqtt.

---
+++ onsmqtt/src/model/GetDeviceCredentialResult.cc
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <alibabacloud/onsmqtt/model/GetDeviceCredentialResult.h>
+#include <json/json.h>
+
+using namespace AlibabaCloud::OnsMqtt;
+using namespace AlibabaCloud::OnsMqtt::Model;
+
+GetDeviceCredentialResult::GetDeviceCredentialResult() :
+	ServiceResult()
+{}
+
+GetDeviceCredentialResult::GetDeviceCredentialResult(const std::string &payload) :
+	ServiceResult()
+{
+	parse(payload);
+}
+
+GetDeviceCredentialResult::~GetDeviceCredentialResult()
+{}
+
+void GetDeviceCredentialResult::parse(const std::string &payload)
+{
+	Json::Reader reader;
+	Json::Value value;
+	reader.parse(payload, value);
+	setRequestId(value["RequestId"].asString());
+	auto deviceCredentialNode = value["DeviceCredential"];
+	if(!deviceCredentialNode["ClientId"].isNull())
+		deviceCredential_.clientId = deviceCredentialNode["ClientId"].asString();
+	if(!deviceCredentialNode["DeviceAccessKeyId"].isNull())
+		deviceCredential_.deviceAccessKeyId = deviceCredentialNode["DeviceAccessKeyId"].asString();
+	if(!deviceCredentialNode["DeviceAccessKeySecret"].isNull())
+		deviceCredential_.deviceAccessKeySecret = deviceCredentialNode["DeviceAccessKeySecret"].asString();
+	if(!deviceCredentialNode["InstanceId"].isNull())
+		deviceCredential_.instanceId = deviceCredentialNode["InstanceId"].asString();
+	if(!deviceCredentialNode["CreateTime"].isNull())
+		deviceCredential_.createTime = std::stol(deviceCredentialNode["CreateTime"].asString());
+	if(!deviceCredentialNode["UpdateTime"].isNull())
+		deviceCredential_.updateTime = std::stol(deviceCredentialNode["UpdateTime"].asString());
+
+}
+
+GetDeviceCredentialResult::DeviceCredential GetDeviceCredentialResult::getDeviceCredential()const
+{
+	return deviceCredential_;
+}
+
---
+++ onsmqtt/src/model/RegisterDeviceCredentialRequest.cc
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2009-2017 Alibaba Cloud All rights reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <alibabacloud/onsmqtt/model/RegisterDeviceCredentialRequest.h>
+
+using AlibabaCloud::OnsMqtt::Model::RegisterDeviceCredentialRequest;
+
+RegisterDeviceCredentialRequest::RegisterDeviceCredentialRequest() :
+	RpcServiceRequest("onsmqtt", "2020-04-20", "RegisterDeviceCredential")
+{
+	setMethod(HttpRequest::Method::Post);
+}
+
+RegisterDeviceCredentialRequest::~RegisterDeviceCredentialRequest()
+{}
+
+std::string RegisterDeviceCredentialRequest::getClientId()const
+{
+	return clientId_;
+}
+
+void RegisterDeviceCredentialRequest::setClientId(const std::string& clientId)
+{
+	clientId_ = clientId;
+	setParameter("ClientId", clientId);
+}
+
+std::string RegisterDeviceCredentialRequest::getInstanceId()const
+{
+	return instanceId_;
+}
+
+void RegisterDeviceCredentialRequest::setInstanceId(const std::string& instanceId)
+{
+	instanceId_ = instanceId;
+	setParameter("InstanceId", instanceId);
+}
+
