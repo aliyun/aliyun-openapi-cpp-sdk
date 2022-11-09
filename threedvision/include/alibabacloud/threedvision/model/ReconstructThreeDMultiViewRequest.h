dce3c275472d28b644b0b4538084883df11e59a2
sdk-team <sdk-team@alibabacloud.com>
2022-11-09 06:40:47 +0000

    Release EstimateMonocularVideoDepth.

--- threedvision/include/alibabacloud/threedvision/model/ReconstructThreeDMultiViewRequest.h
+++ threedvision/include/alibabacloud/threedvision/model/ReconstructThreeDMultiViewRequest.h
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009-2017 Alibaba Cloud All rights reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,38 +17,32 @@
 #ifndef ALIBABACLOUD_THREEDVISION_MODEL_RECONSTRUCTTHREEDMULTIVIEWREQUEST_H_
 #define ALIBABACLOUD_THREEDVISION_MODEL_RECONSTRUCTTHREEDMULTIVIEWREQUEST_H_
 
+#include <alibabacloud/threedvision/ThreedvisionExport.h>
+#include <alibabacloud/core/RpcServiceRequest.h>
 #include <string>
 #include <vector>
-#include <alibabacloud/core/RpcServiceRequest.h>
-#include <alibabacloud/threedvision/ThreedvisionExport.h>
+#include <map>
 
-namespace AlibabaCloud
-{
-	namespace Threedvision
-	{
-		namespace Model
-		{
-			class ALIBABACLOUD_THREEDVISION_EXPORT ReconstructThreeDMultiViewRequest : public RpcServiceRequest
-			{
+namespace AlibabaCloud {
+namespace Threedvision {
+namespace Model {
+class ALIBABACLOUD_THREEDVISION_EXPORT ReconstructThreeDMultiViewRequest : public RpcServiceRequest {
+public:
+	ReconstructThreeDMultiViewRequest();
+	~ReconstructThreeDMultiViewRequest();
+	std::string getMode() const;
+	void setMode(const std::string &mode);
+	std::string getZipFileUrl() const;
+	void setZipFileUrl(const std::string &zipFileUrl);
+	bool getAsync() const;
+	void setAsync(bool async);
 
-			public:
-				ReconstructThreeDMultiViewRequest();
-				~ReconstructThreeDMultiViewRequest();
-
-				std::string getMode()const;
-				void setMode(const std::string& mode);
-				std::string getZipFileUrl()const;
-				void setZipFileUrl(const std::string& zipFileUrl);
-				bool getAsync()const;
-				void setAsync(bool async);
-
-            private:
-				std::string mode_;
-				std::string zipFileUrl_;
-				bool async_;
-
-			};
-		}
-	}
-}
+private:
+	std::string mode_;
+	std::string zipFileUrl_;
+	bool async_;
+};
+} // namespace Model
+} // namespace Threedvision
+} // namespace AlibabaCloud
 #endif // !ALIBABACLOUD_THREEDVISION_MODEL_RECONSTRUCTTHREEDMULTIVIEWREQUEST_H_
