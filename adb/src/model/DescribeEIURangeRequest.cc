a09f60b5424413f522dae438efed28533c54d5a5
sdk-team <sdk-team@alibabacloud.com>
2023-08-01 02:27:43 +0000

    Support resource group id.

--- adb/src/model/DescribeEIURangeRequest.cc
+++ adb/src/model/DescribeEIURangeRequest.cc
@@ -43,6 +43,15 @@ void DescribeEIURangeRequest::setAccessKeyId(const std::string &accessKeyId) {
   setParameter(std::string("AccessKeyId"), accessKeyId);
 }
 
+std::string DescribeEIURangeRequest::getResourceGroupId() const {
+  return resourceGroupId_;
+}
+
+void DescribeEIURangeRequest::setResourceGroupId(const std::string &resourceGroupId) {
+  resourceGroupId_ = resourceGroupId;
+  setParameter(std::string("ResourceGroupId"), resourceGroupId);
+}
+
 std::string DescribeEIURangeRequest::getRegionId() const {
   return regionId_;
 }
